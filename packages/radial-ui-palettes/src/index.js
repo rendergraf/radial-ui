f7023267e581a65ac69b39379fbe8102b710fe75
Xavier Araque <xavier.araque@alten.es>
2023-11-29 18:18:51 +0100

    Fix colors generate

--- packages/radial-ui-palettes/src/index.js
+++ packages/radial-ui-palettes/src/index.js
@@ -1,148 +1,182 @@
 import * as fs from "fs";
 import tinycolor from "tinycolor2";
+import { colorPalettes, opacityValues } from "./colorData.js";
 
-const colorData = {
-  "gray": "FFFFFF",
-  "tomato": "#FFFCFC",
-  "red": "#FFFCFC",
-	"ruby": "#FFFCFD",
-  "pink": "#FFFCFE",
-  "plum": "#FEFCFF",
-  "purple": "#FEFCFE",
-  "violet": "#FDFCFE",
-  "iris": "#FDFDFF",
-  "indigo": "#FDFDFE",
-  "blue": "#FBFDFF",
-  "cyan": "#fafdfe",
-  "teal": "#FAFEFD",
-  "jade": "#FBFEFD",
-  "green": "#FBFEFC",
-  "grass": "#FBFEFB",
-  "bronze": "#FDFCFC",
-  "gold": "#FDFDFC",
-  "brown": "#FEFDFC",
-  "orange": "#FEFCFB",
-  "amber": "#FEFDFB",
-  "yellow": "#FFFFEE",
-  "lime": "#FCFDFA",
-  "mint": "#F9FEFD",
-  "sky": "#F9FEFF"
-};
+function generateColorRanges(colorPalettes) {
+  const colorRanges = {};
 
-function getColorInfo(hexColor) {
-  const rgbColor = tinycolor(hexColor).toRgb();
-  return {
-    rgb: `${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}`,
-	rgb8: tinycolor(hexColor).toHex8String(),
-	rgbObject: rgbColor,
-  };
-}
-
-function colorDisplay(rgbColor) {
-  const toPercentage = (component) => (component === 255 ? '1' : (component / 255).toFixed(3));
-  const p3Color = `${toPercentage(rgbColor.r)} ${toPercentage(rgbColor.g)} ${toPercentage(rgbColor.b)}`;
-  return p3Color;
-}
-
-/* function generateHexColorVariables(hexColor, color, steps = 12, darkenFactor = 1.1) {
-  const baseColor = tinycolor(hexColor);
-  let cssContent = '';
-
-  for (let i = 0; i < steps; i++) {
-    let factor = i === 10 ? 2 : darkenFactor;
-    const nextColor = baseColor.darken(factor * i).saturate().toHexString();
-    cssContent += `--${color}${i + 1}: ${nextColor};\n`;
-  }
-
-  return cssContent;
-} */
-
-function generateHexColorVariables(hexColor, color, steps = 12, darkenFactor = 1.1) {
-  const baseColor = tinycolor(hexColor);
-  let cssContent = '';
-
-  for (let i = 0; i < steps; i++) {
-    let modifiedColor = baseColor;
-    let factor = i === 10 ? 2 : darkenFactor;
-    if (color === 'gray') {
-      modifiedColor = baseColor.darken((factor + 0.2) * i).saturate().greyscale();
-    } else {
-      modifiedColor = baseColor.darken(factor * i).saturate();
-    }
-
-    const nextColor = modifiedColor.toHexString();
-    cssContent += `--${color}${i + 1}: ${nextColor};\n`;
-  }
-
-  return cssContent;
-}
-
-
-function generateHex8ColorVariables(hexColor, color, steps = 12, darkenFactor = 1.2) {
-	// ${opacityValues.map((opacity, index) => `--${color}-a${index}: ${variants.HEX8};`).join('\n')}
-  const baseColor = tinycolor(hexColor);
-  let cssContent = '';
-
-  for (let i = 0; i < steps; i++) {
-    const nextColor = baseColor.darken(darkenFactor * i).toHex8String();
-    cssContent += `--${color}-a${i + 1}: ${nextColor};\n`;
-  }
-
-  return cssContent;
-}
-
-function colorVariants(colorData) {
-  const variants = {};
-
-  Object.entries(colorData).forEach(([color, hex]) => {
-    variants[color] = {
-    	"A": `rgba(${getColorInfo(hex).rgb}`,
-    	"P3A": `color(display-p3 ${colorDisplay(getColorInfo(hex).rgbObject)}`,
-		"HEX": hex
+  Object.entries(colorPalettes).forEach(([colorName, colorHex]) => {
+    colorRanges[colorName] = {
+      ranges: generateColor(colorHex)
     };
   });
 
-  return variants;
+  return colorRanges;
 }
 
-const opacityValues = [".05", ".1", ".15", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", ".95"];
+function generateColor(baseColorHex) {
+  const steps = 12;
+  const darkenFactor = 1.1;
+  const baseColor = tinycolor(baseColorHex);
+  const colorArray = [];
 
-const colorVariantsObj = colorVariants(colorData);
+  for (let i = 0; i < steps; i++) {
+    const factor = i === 10 ? 2 : darkenFactor;
+    const modifiedColor = calculateModifiedColor(baseColor, factor, i);
+    const nextColor = modifiedColor.toHexString();
+    colorArray.push(nextColor);
+  }
 
-generateAndWriteCSSFiles(colorVariantsObj, opacityValues);
-
-function generateAndWriteCSSFiles(colorVariants, opacityValues) {
-  Object.entries(colorVariants).forEach(([color, variants]) => {
-    const cssContent = generateCSSVariables(variants, opacityValues, color);
-    const fileName = `${color}.css`;
-    fs.writeFileSync(`./dist/${fileName}`, cssContent, 'utf-8');
-    console.log(`Archivo ${fileName} generado con éxito.`);
-  });
+  return colorArray;
 }
 
-function generateCSSVariables(variants, opacityValues, color) {
+function calculateModifiedColor(color, factor, index, colorName) {
+  if (colorName === "gray" && index === 10) {
+    return color.darken((factor + 0.2) * index).saturate().greyscale();
+  }
+  return color.darken(factor * index).saturate();
+}
+
+function generateCSSVariables(colorName, colorData) {
+  const { ranges } = colorData;
+  const rootVars = generateVariables(colorName, ranges);
   return `
+/* Hex colors */
 :root {
-${opacityValues.map((opacity, index) => `--${color}-a${index + 1}: ${variants.A}, ${opacity});`).join('\n')}
+${rootVars}
+}
+`;
 }
 
-:root, .light, .light-theme {
-/* Hex */
-${generateHexColorVariables(variants.HEX, color)}
-/* Hex8 */
-${generateHex8ColorVariables(variants.HEX, color)}
+function generateVariables(colorName, colorRanges) {
+  const rootVars = colorRanges.map((color, index) => {
+    const currentColor = tinycolor(color);
+    const modifiedColor = colorName === "gray" ? currentColor.greyscale().toHexString() : currentColor.toHexString();
+    return `--${colorName}-${index + 1}: ${modifiedColor};\n`;
+  });
+
+  return rootVars.join('');
 }
 
+function generateCSSVariablesHex8(colorName, colorData) {
+  const { ranges } = colorData;
+  const rootVars = generateVariablesHex8(colorName, ranges);
+  return `
+/* Hex 8 colors */
+:root {
+${rootVars}
+}
+`;
+}
+
+function generateVariablesHex8(colorName, colorRanges) {
+  const rootVars = colorRanges.map((color, index) => {
+    const currentColor = tinycolor(color);
+    const modifiedColor = colorName === "gray" ? currentColor.greyscale().toHex8String() : currentColor.toHex8String();
+    return `--${colorName}-${index + 1}: ${modifiedColor};\n`;
+  });
+
+  return rootVars.join('');
+}
+
+function hexToP3(hexColor) {
+  if (typeof hexColor !== 'string' || hexColor.charAt(0) !== '#' || hexColor.length !== 7) {
+    throw new Error('Invalid hex string!');
+  }
+
+  const parsedColor = [
+    parseInt(hexColor.substring(1, 3), 16) / 255,
+    parseInt(hexColor.substring(3, 5), 16) / 255,
+    parseInt(hexColor.substring(5, 7), 16) / 255
+  ];
+
+  const formattedColor = `color(display-p3 ${parsedColor[0].toFixed(2)} ${parsedColor[1].toFixed(2)} ${parsedColor[2].toFixed(2)} / 1)`;
+
+  return formattedColor;
+}
+
+function generateVariablesP3(colorName, colorRanges) {
+  const rootVars = colorRanges.map((color, index) => {
+    return `--${colorName}-${index + 1}: ${hexToP3(color)};\n`;
+  });
+
+  return rootVars.join('');
+}
+
+function hexToP3Alpha(hexColor, alpha) {
+  if (typeof hexColor !== 'string' || hexColor.charAt(0) !== '#' || hexColor.length !== 7) {
+    throw new Error('Invalid hex string!');
+  }
+
+  const parsedColor = [
+    parseInt(hexColor.substring(1, 3), 16) / 255,
+    parseInt(hexColor.substring(3, 5), 16) / 255,
+    parseInt(hexColor.substring(5, 7), 16) / 255
+  ];
+
+  const formattedColor = `color(display-p3 ${parsedColor[0].toFixed(2)} ${parsedColor[1].toFixed(2)} ${parsedColor[2].toFixed(2)} / ${alpha})`;
+
+  return formattedColor;
+}
+
+function generateVariablesP3A(colorName, colorRanges) {
+  const rootVars = colorRanges.map((color, index) => {
+    const alpha = opacityValues[index];
+    return `--${colorName}-A${index + 1}: ${hexToP3Alpha(color, alpha)};\n`;
+  });
+
+  return rootVars.join('');
+}
+
+function generateCSSVariablesP3(colorName, colorData) {
+  const { ranges } = colorData;
+  const rootVars = generateVariablesP3(colorName, ranges);
+  return `
+/* Display P3 */
 @supports (color: color(display-p3 1 1 1)) and (color-gamut: p3) {
-  :root {
-/* Color Gamut and Display P3 */
-${opacityValues.map((opacity, index) => `--${color}${index + 1}: ${variants.P3A});`).join('\n')}
-
-/* Color Gamut and Display P3 alpha */
-${opacityValues.map((opacity, index) => `--${color}-a${index + 1}: ${variants.P3A} / ${opacity});`).join('\n')}
+:root {
+${rootVars}
   }
 }
 `;
 }
 
-export {};
+function generateCSSVariablesP3A(colorName, colorData) {
+  const { ranges } = colorData;
+  const rootVars = generateVariablesP3A(colorName, ranges);
+  return `
+/* Display P3 Alpha*/
+@supports (color: color(display-p3 1 1 1)) and (color-gamut: p3) {
+:root {
+${rootVars}
+  }
+}
+`;
+}
+
+function generateCSS(colorPalettes) {
+  const ranges = generateColorRanges(colorPalettes);
+
+  Object.entries(ranges).forEach(([colorName, colorData]) => {
+    const cssContent = combineCSSContent(colorName, colorData);
+    writeCSSFiles(colorName, cssContent);
+  });
+}
+
+function combineCSSContent(colorName, colorData) {
+  const cssContents = [
+    generateCSSVariables(colorName, colorData),
+    generateCSSVariablesHex8(colorName, colorData),
+    generateCSSVariablesP3(colorName, colorData),
+    generateCSSVariablesP3A(colorName, colorData)
+  ];
+  return cssContents.join("\n\n");
+}
+
+function writeCSSFiles(colorName, cssContent) {
+  const fileName = `${colorName}.css`;
+  fs.writeFileSync(`./dist/${fileName}`, cssContent, "utf-8");
+  console.log(`Archivo ${fileName} generado con éxito.`);
+}
+
+generateCSS(colorPalettes);
